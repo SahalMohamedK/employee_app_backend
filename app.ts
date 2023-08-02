2ad55f9e02155c82eb92b5683ef5861e85cad68c
Sahal Mohamed <sahalkunnummal@gmail.com>
2023-08-02 16:59:01 +0530

    impliment type orm

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
+import "reflect-metadata"
 import express from 'express'
 import employeeRouter from './employee_router';
 import loggerMiddleware from './loggerMiddleware';
+import dataSource from "./dataSource";
 
 const server = express();
 
@@ -8,6 +10,10 @@ server.use(express.json())
 server.use(loggerMiddleware);
 server.use('/employees', employeeRouter);
 
-server.listen(3000, () => {
-    console.log("server is listening to port 3000");
-})
+(async () => {
+    await dataSource.initialize()
+    server.listen(3000, () => {
+        console.log("server is listening to port 3000");
+    })
+})()
+
--- employee_router.ts
+++ employee_router.ts
@@ -1,35 +1,22 @@
 import express from "express"
 import Employee from "./Employee";
+import dataSource from "./dataSource";
 
 const employeeRouter = express.Router();
 
-let count = 2;
+employeeRouter.get('', async (req, res) => {
+    const employeeRepo = dataSource.getRepository(Employee)
+    const employees = await employeeRepo.find()
 
-const employees: Employee[] = [
-    {
-        id: 1,
-        name: "Sahal Mohamed",
-        email: "sahalkunnummal@gmail.com",
-        createdAt: new Date(),
-        updatedAt: new Date()
-    },
-    {
-        id: 2,
-        name: "Harry Potter",
-        email: "harrypotter@gmail.com",
-        createdAt: new Date(),
-        updatedAt: new Date()
-    }
-]
-
-
-employeeRouter.get('', (req, res) => {
     res.status(200).send(employees)
 })
 
-employeeRouter.get('/:id', (req, res) => {
+employeeRouter.get('/:id', async(req, res) => {
     const id:number = Number(req.params.id)
-    const employee = employees.find(employee => employee.id === id)
+    
+    const employeeRepo = dataSource.getRepository(Employee)
+    const employee = await employeeRepo.findOneBy({id})
+
     if(employee){
         res.status(200).send(employee)
     }else{
@@ -37,38 +24,45 @@ employeeRouter.get('/:id', (req, res) => {
     }
 })
 
-employeeRouter.post('', (req, res) => {
+employeeRouter.post('', async(req, res) => {
     const employee = new Employee();
-    employee.id = ++count;
     employee.name = req.body.name;
     employee.email = req.body.email;
-    employee.createdAt = new Date()
-    employee.updatedAt = new Date()
 
-    employees.push(employee);
+    const employeeRepo = dataSource.getRepository(Employee)
+    const saveEmployee = await employeeRepo.save(employee)
 
-    res.status(200).send(employee)
+    res.status(200).send(saveEmployee)
 })
 
-employeeRouter.put('/:id', (req, res) => {
+employeeRouter.put('/:id', async (req, res) => {
     const id = Number(req.params.id)
-    const employee = employees.find(employee => employee.id === id)
-    employee.name = req.body.name
-    employee.email = req.body.email
-    employee.updatedAt = new Date()
 
-    res.status(200).send(employee)
-})
-
-employeeRouter.delete('/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const idx = employees.findIndex(employee => employee.id === id)
-    if(idx === -1){
-        res.status(400).send({message: `No employee with the id ${id}`})
+    const employeeRepo = dataSource.getRepository(Employee)
+    const employee = await employeeRepo.findOneBy({id})
+    if(employee){
+        employee.name = req.body.name
+        employee.email = req.body.email
+        const saveEmployee = await employeeRepo.save(employee)
+    
+        res.status(200).send(saveEmployee)
     }else{
-        employees.splice(idx, 1)
-        
+        res.status(400).send({message: `No employee with the id ${id}`})
+    }
+
+})
+
+employeeRouter.delete('/:id', async (req, res) => {
+    const id = Number(req.params.id)
+    
+    const employeeRepo = dataSource.getRepository(Employee)
+    const employee = await employeeRepo.findOneBy({id})
+
+    if(employee){
+        await employeeRepo.remove(employee)
         res.status(200).send({message: `Employee with id ${id} deleted`})
+    }else{
+        res.status(400).send({message: `No employee with the id ${id}`})
     }
 })
 
